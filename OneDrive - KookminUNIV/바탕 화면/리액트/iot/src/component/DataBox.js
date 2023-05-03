4fb0a9cb1767595c8b08a8b27465d11f86a3ef67
SunJin Moon <111959522+anjdfk111@users.noreply.github.com>
2023-05-03 16:11:59 +0900

    first commit

---
+++ OneDrive - KookminUNIV/바탕 화면/리액트/iot/src/component/DataBox.js
@@ -0,0 +1,115 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+
+import { Box, Typography, Button, Grid } from '@material-ui/core';
+
+//axios
+import axios from 'axios';
+
+//redux
+import { useSelector, useDispatch } from 'react-redux';
+import { updateHumidity, updateSoil, updateTemp } from '../store';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+export default function DataBox() {
+  //redux
+  const dispatch = useDispatch();
+  let soilMoisture = useSelector((state) => state.soilMoisture);
+  let temp = useSelector((state) => state.temp);
+  let humidity = useSelector((state) => state.humidity);
+
+  //dispatch가 실행 때마다 data 업데이트 해서 보여주는 코드
+  //useEffect의 의존성 배열(두번째 매개변수)에 dispatch
+  React.useEffect(() => {
+    axios.get('http://localhost:3001/dummy').then((result) => {
+      //일단 0시의 temp와 humidity
+      let moistValue = result.data[1].data[0].y;
+      let tempValue = result.data[0].data[0].y;
+      let humidityValue = result.data[1].data[0].y;
+
+      //Dispatch
+      dispatch(updateSoil(moistValue));
+      dispatch(updateTemp(tempValue));
+      dispatch(updateHumidity(humidityValue));
+    });
+  }, [dispatch]);
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid
+        container
+        spacing={{ xs: 1, md: 3 }}
+        columns={{ xs: 1, sm: 4, md: 12 }}
+        justifyContent="center"
+      >
+        <Grid>
+          <Item
+            sx={{
+              m: 5,
+              width: 300,
+              height: 180,
+              border: '2px solid #E1E2E3 ',
+            }}
+          >
+            <h1>Humidity</h1>
+            <Typography variant="h5" component="div">
+              {humidity}
+            </Typography>
+          </Item>
+        </Grid>
+        <Grid>
+          <Item
+            sx={{
+              m: 5,
+              width: 300,
+              height: 180,
+              border: '2px solid #E1E2E3 ',
+            }}
+          >
+            <h1>Temperature</h1>
+            <Typography variant="h5" component="div">
+              {temp}
+            </Typography>
+          </Item>
+        </Grid>
+
+        <Grid>
+          <Item
+            sx={{
+              m: 5,
+              width: 300,
+              height: 180,
+              border: '2px solid #E1E2E3 ',
+            }}
+          >
+            <h1>Soil-Moisture</h1>
+            <Typography variant="h5" component="div">
+              {soilMoisture}
+            </Typography>
+          </Item>
+        </Grid>
+        <Grid>
+          <Item
+            sx={{
+              m: 5,
+              width: 300,
+              height: 180,
+              border: '2px solid #E1E2E3 ',
+            }}
+          >
+            <h1>Amount of sunshine</h1>
+            <Typography variant="h5" component="div"></Typography>
+          </Item>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
---
+++ OneDrive - KookminUNIV/바탕 화면/리액트/iot/src/component/DataChart.js
@@ -0,0 +1,90 @@
+import React, { useState, useEffect } from 'react';
+import { ResponsiveLine } from '@nivo/line';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+
+const Chart = () => {
+  const type = useParams();
+  const dataType = type.id;
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await axios.get('http://localhost:3001/dummy');
+      setData(response.data);
+    }
+    fetchData();
+  }, []);
+  const chartData = data.filter((id) => id.id === dataType);
+  return (
+    <div style={{ width: 'auto', height: '400px', margin: '0 auto' }}>
+      <ResponsiveLine
+        data={chartData}
+        margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+        xScale={{ type: 'point' }}
+        yScale={{
+          type: 'linear',
+          min: 'auto',
+          max: 'auto',
+          stacked: true,
+          reverse: false,
+        }}
+        yFormat=" >-.2f"
+        axisTop={null}
+        axisRight={null}
+        axisBottom={{
+          orient: 'bottom',
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: 'transportation',
+          legendOffset: 36,
+          legendPosition: 'middle',
+        }}
+        axisLeft={{
+          orient: 'left',
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: 'count',
+          legendOffset: -40,
+          legendPosition: 'middle',
+        }}
+        pointSize={10}
+        pointColor={{ theme: 'background' }}
+        pointBorderWidth={2}
+        pointBorderColor={{ from: 'serieColor' }}
+        pointLabelYOffset={-12}
+        useMesh={true}
+        legends={[
+          {
+            anchor: 'bottom-right',
+            direction: 'column',
+            justify: false,
+            translateX: 100,
+            translateY: 0,
+            itemsSpacing: 0,
+            itemDirection: 'left-to-right',
+            itemWidth: 80,
+            itemHeight: 20,
+            itemOpacity: 0.75,
+            symbolSize: 12,
+            symbolShape: 'circle',
+            symbolBorderColor: 'rgba(0, 0, 0, .5)',
+            effects: [
+              {
+                on: 'hover',
+                style: {
+                  itemBackground: 'rgba(0, 0, 0, .03)',
+                  itemOpacity: 1,
+                },
+              },
+            ],
+          },
+        ]}
+      />
+    </div>
+  );
+};
+
+export default Chart;
